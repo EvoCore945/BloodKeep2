9589eb394c9be2628a211db7ae8917028526c41c
hofman5 <zstrafer1232@centrum.cz>
2025-04-24 11:26:36 +0200

    command pattern

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import World.WorldMap;
 
 import java.io.FileNotFoundException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
